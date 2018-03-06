794e7cc5bcd2fc355ada0e17eef2223e669c4c4d
francisLiuX <liupabshi90@126.com>
2018-03-06 14:01:13 +0800

    project4-0306

--- RZ/js/festival.js
+++ RZ/js/festival.js
@@ -2,11 +2,27 @@ $(()=>{
     $.get("data/getfestival.php").then(data=>{
         "use strict";
         var html="";
-        for(var item of data){
-            html+=`<img src="item.pic
-" alt="">`;
+        console.log(1)
+        // for(var item of data){
+        //     console.log(data.length)
+        //     console.log(item.pic)
+        //     html+=`<a href="" target="_blank" class="inner_picture">
+        //                         <img src="${item.pic}" alt="">
+        //                     </a>`;
+        // }
+        for(var i=0;i<data.length;i++){
+            // console.log(data)
+            console.log(data[0])
+            // console.log(data[i].pic)
+            html+=` <li class="inner_item">
+                        <a href="" target="_blank" class="inner_picture">
+                                <img src="${data[i].pic}" alt="">
+                            </a>
+                     </li>`;
         }
-        var $img=$(".inner_picture");
+        console.log(2)
+        var $img=$(".inner_list");
+        console.log(3);
         $img.html(html);
     })
 })
