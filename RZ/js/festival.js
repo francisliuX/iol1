52bb4b155c998b1b31c12159a4f31d6577dca9cc
Administrator <liupabshi90@126.com>
2018-03-06 10:47:50 +0800

    project4-0306

--- RZ/js/festival.js
+++ RZ/js/festival.js
@@ -1,3 +1,12 @@
 $(()=>{
-    $.get("data/getfestival.php")
+    $.get("data/getfestival.php").then(data=>{
+        "use strict";
+        var html="";
+        for(var item of data){
+            html+=`<img src="item.pic
+" alt="">`;
+        }
+        var $img=$(".inner_picture");
+        $img.html(html);
+    })
 })
